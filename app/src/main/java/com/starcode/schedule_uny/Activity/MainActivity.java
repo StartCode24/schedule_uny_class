312df116a21e52c996a40b79fb7f05b5ba9ec0bb
Prayugo Dwi <Dwi.deni24@gmail.com>
2018-08-28 11:18:24 +0700

    Dev pertama

---
+++ app/src/main/java/com/starcode/schedule_uny/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.starcode.schedule_uny.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.starcode.schedule_uny.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
