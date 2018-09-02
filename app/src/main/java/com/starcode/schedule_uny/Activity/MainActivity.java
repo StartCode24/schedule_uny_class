cd1a61a72b57e92def421ce90d09ec650819178d
Prayugo Dwi <Dwi.deni24@gmail.com>
2018-09-02 22:05:49 +0700

    penambahan fitur setting dan action navigation
    penambahan fitur setting dan action navigation

--- app/src/main/java/com/starcode/schedule_uny/Activity/MainActivity.java
+++ app/src/main/java/com/starcode/schedule_uny/Activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.starcode.schedule_uny.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.starcode.schedule_uny.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    void signIn(){
+        startActivity(new Intent(MainActivity.this, Home_activity.class).
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+        finish();
     }
 }
--- app/src/main/java/com/starcode/schedule_uny/Activity/Setting_Activity.java
+++ app/src/main/java/com/starcode/schedule_uny/Activity/Setting_Activity.java
@@ -1,6 +1,5 @@
 package com.starcode.schedule_uny.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,15 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.starcode.schedule_uny.R;
-
-public class Home_activity extends AppCompatActivity
+public class Setting_Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_activity);
+        setContentView(R.layout.activity_setting_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -58,7 +55,7 @@ public class Home_activity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_activity, menu);
+        getMenuInflater().inflate(R.menu.setting_, menu);
         return true;
     }
 
@@ -83,11 +80,18 @@ public class Home_activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_schedule) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_setting) {
-            startActivity(new Intent(Home_activity.this, Setting_Activity.class).
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
