5f726fc7835991bed1365fdb0f3c40401f9f6ae7
Prayugo Dwi <Dwi.deni24@gmail.com>
2018-09-11 21:06:48 +0700

    modify_login
    modify_login

--- app/src/main/java/com/starcode/schedule_uny/Activity/MainActivity.java
+++ app/src/main/java/com/starcode/schedule_uny/Activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.starcode.schedule_uny.Activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.starcode.schedule_uny.R;
 import butterknife.BindView;
@@ -14,17 +18,40 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.rellay1)
+    RelativeLayout rellay1;
+    @BindView(R.id.rellay2)
+    RelativeLayout rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
     }
 
-    @OnClick(R.id.btnLogin)
-    void signIn(){
-        startActivity(new Intent(MainActivity.this, Home_activity.class).
-                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
-        finish();
+    @OnClick(R.id.btn_login)
+    void btn_login(){
+        Toast.makeText(MainActivity.this,"Login",Toast.LENGTH_SHORT).show();;
     }
+
+    @OnClick(R.id.btn_fgtpassword)
+    void btn_fgtpassword(){
+        Toast.makeText(MainActivity.this,"Forget Password",Toast.LENGTH_SHORT).show();;
+    }
+
+
 }
